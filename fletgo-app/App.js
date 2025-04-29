1e4a9f344b5b7ebe877eaf89a8e861151b9792ad
said199 <61359272+said199@users.noreply.github.com>
2025-04-29 15:21:58 -0600

    Add fletgo-app/App.js 1745961717836

--- fletgo-app/App.js
+++ fletgo-app/App.js
@@ -9,10 +9,12 @@ import { UserModeProvider } from './src/context/UserModeContext';
 import { UserProvider } from './src/context/UserContext';
 import LoginScreen from './src/screens/LoginScreen';
 import RegisterScreen from './src/screens/RegisterScreen';
+import VerificationScreen from './src/screens/VerificationScreen';
 import HomeScreen from './src/screens/HomeScreen';
 import { EditProfileScreen } from './src/screens/EditProfileScreen';
 import { DrawerContent } from './src/navigation/DrawerContent';
 import { DriverVerificationScreen } from './src/screens/DriverVerificationScreen';
+import { BasicInfoScreen } from './src/screens/BasicInfoScreen';
 
 const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
@@ -41,6 +43,14 @@ function HomeDrawer() {
           drawerItemStyle: { height: 0 }
         }}
       />
+      <Drawer.Screen 
+        name="BasicInfo" 
+        component={BasicInfoScreen}
+        options={{
+          drawerLabel: () => null,
+          drawerItemStyle: { height: 0 }
+        }}
+      />
     </Drawer.Navigator>
   );
 }
@@ -59,6 +69,7 @@ export default function App() {
             >
               <Stack.Screen name="Login" component={LoginScreen} />
               <Stack.Screen name="Register" component={RegisterScreen} />
+              <Stack.Screen name="Verification" component={VerificationScreen} />
               <Stack.Screen name="Home" component={HomeDrawer} />
             </Stack.Navigator>
           </NavigationContainer>
